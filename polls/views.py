58f21540c579f3b9cf80595ea43f911416ca0883
Ping Lee <hackercollector4@gmail.com>
2018-08-09 12:28:22 +0800

    - coverage integrated - DetailViewTest added - requrements.txt added

--- polls/views.py
+++ polls/views.py
@@ -19,6 +19,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
+
+    def get_queryset(self):
+        """
+        Exclude any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
     
 class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
